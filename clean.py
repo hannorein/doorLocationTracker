79fae180b4434a0b3fc217ba9de9c3fc7a1109c5
Hanno Rein <hanno@hanno-rein.de>
2018-08-17 17:19:01 -0400

    webform

--- clean.py
+++ clean.py
@@ -1,9 +1,17 @@
 sfilen = "/home/rein/git/doorLocationTracker/status.html"
+sfilen2 = "/home/rein/git/doorLocationTracker/status_base.html"
 sstr0 = "<dd><span class=\"week\">"
 with open(sfilen) as f:
     sf = f.readlines()
 
 with open(sfilen,"w") as f:
+    for r in sf:
+        for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]:
+            sstr = sstr0 + day
+            if sstr in r:
+                r = "<dd><span class=\"week\">"+day+":</span> 		<span class=\"tbd\"></span></dd>\n"
+        f.write(r)
+with open(sfilen2,"w") as f:
     for r in sf:
         for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]:
             sstr = sstr0 + day
